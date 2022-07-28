30f29c60fd097fb4b812f31b4328cc41f103d033
Luiz Felipe <luizfelipepintorr@gmail.com>
2022-07-28 13:55:04 -0300

    Criando o Projeto

---
+++ src/main/java/com/example/Projeto_web_services_com_Spring_Boot_e_JPA_Hibernate/ProjetoWebServicesComSpringBootEJpaHibernateApplication.java
@@ -0,0 +1,13 @@
+package com.example.Projeto_web_services_com_Spring_Boot_e_JPA_Hibernate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoWebServicesComSpringBootEJpaHibernateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoWebServicesComSpringBootEJpaHibernateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Projeto_web_services_com_Spring_Boot_e_JPA_Hibernate/ProjetoWebServicesComSpringBootEJpaHibernateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Projeto_web_services_com_Spring_Boot_e_JPA_Hibernate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoWebServicesComSpringBootEJpaHibernateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
