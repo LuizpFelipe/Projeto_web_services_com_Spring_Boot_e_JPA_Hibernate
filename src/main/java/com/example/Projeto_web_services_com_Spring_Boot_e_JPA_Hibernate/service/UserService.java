e390e864e1ea748f7e67dddeef4a991db3c1aeb5
Luiz Felipe <luizfelipepintorr@gmail.com>
2022-07-29 21:36:13 -0300

    Service layer, componente de Resgistro

---
+++ src/main/java/com/example/Projeto_web_services_com_Spring_Boot_e_JPA_Hibernate/service/UserService.java
@@ -0,0 +1,29 @@
+package com.example.Projeto_web_services_com_Spring_Boot_e_JPA_Hibernate.service;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.Projeto_web_services_com_Spring_Boot_e_JPA_Hibernate.entities.User;
+import com.example.Projeto_web_services_com_Spring_Boot_e_JPA_Hibernate.repository.UserRepository;
+
+
+@Service
+public class UserService {
+	
+	@Autowired
+	private UserRepository repository;
+	
+	
+	public List<User> findAll(){
+		return repository.findAll();
+	}
+	
+	public User findById(Long id) {
+		Optional<User> obj = repository.findById(id);
+		return obj.get();
+	}
+	
+}
